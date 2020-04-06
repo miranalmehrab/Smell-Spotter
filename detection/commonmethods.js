0d5564be29fbd8a9ff106a00e46c59355d04cedf
MiranAlMehrab <bsse0931@iit.du.ac.bd>
2020-04-06 10:12:57 +0600

    manually creating lexer

---
+++ detection/commonmethods.js
@@ -0,0 +1,40 @@
+function isVarible(type)
+{
+
+}
+
+function isObjectAttributeName()
+{
+
+}
+
+function isDictionaryKeyName()
+{
+
+}
+
+function isCommonPassword()
+{
+
+}
+
+
+function isCommonUserName()
+{
+    
+}
+
+function isCommonTokenName()
+{
+    
+}
+
+function isCommonIDName()
+{
+    
+}
+
+function isLengthZero(word)
+{
+    return word.length>0 ? true : false;
+}
--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 const vscode = require('vscode');
+var fs = require('fs');
 const filbert = require('filbert');
-
-
+const filbert_loose = require('filbert/filbert_loose');
 /**
  * @param {vscode.ExtensionContext} context
  */
@@ -14,25 +14,24 @@ function activate(context) {
 	let parsecode = vscode.commands.registerCommand('extension.parsecode', function () 
 	{
 		const window = vscode.window;
-		var docObj = vscode.window.activeTextEditor.document;
-		var pcode = docObj.getText();
+		var docObj = window.activeTextEditor.document;
+		
+		const pcode = docObj.getText();
 		const codeLang = docObj.languageId;
-		var ast = "";
-
+		
 		if(codeLang === 'python') 
 		{
-			window.showInformationMessage('Creating AST for pyhton code!');
-			if(pcode!=null)
-			{
-				ast = filbert.parse(pcode);
-				window.showInformationMessage('AST created successfully!');
-			}
+			let tokensArr = [];
+			if(pcode!=null) tokensArr = runLexer(pcode);
 			else window.showErrorMessage("Empty source code!");
-			//const ast = filbert.parse(pcode, { locations: true, ranges: true });
+			
+			if(tokensArr) 
+			{
+				console.log(tokensArr);
+				writeTokenToFile(tokensArr);
+			}
 		}
-		console.log(pcode);
-		console.log(ast);
-		
+		else window.showErrorMessage("Please select python source code!");
 	});
 
 	let greetings = vscode.commands.registerCommand('extension.greetings', function()
@@ -44,20 +43,108 @@ function activate(context) {
 	context.subscriptions.push(greetings);
 }
 
-
-function interateThroughObject(obj)
+function writeTokenToFile(tokensArr)
 {
-	for(let key in obj)
-	{
-		let value = obj[key];
-		console.log("key: "+key+" value: "+value);
-		vscode.window.showInformationMessage("key: "+key+" value: "+value);
-		
-		if(typeof value === 'object') interateThroughObject(value);
-		else if(Array.isArray(value)) interateThroughObject(value);
-	}
+	fs.writeFile(__dirname+'/output/output.txt', '', ()=>{console.log('done')})
+	
+	tokensArr.forEach(token => {
+		const value = token.type+","+token.name+","+token.value+"\n";
+		fs.appendFile(__dirname+'/output/output.txt', value, (err) => { 
+			if (err)
+			{
+				console.log(err);
+				throw err;
+			}  
+		})
+	});
+
+	 
 }
 
+function removeUnwantedCharacter(word)
+{
+	console.log(word);
+	const charLength = word.length;
+	const unwantedChars = ["'"];
+	const unwantedCharsCount = unwantedChars.length;
+
+	for(let i=0;i<charLength;i++)
+	{
+		for(let j=0;j<unwantedCharsCount;j++){
+			const unwanted = unwantedChars[j];
+			if(word.includes(unwanted)) word = word.replace(unwanted,'');
+		}
+	}
+	return word;
+}
+
+
+function runLexer(pcode)
+{
+	//const builtInFunctions = ['','','','','','','',''];
+
+	let tokensArr = [];
+	let linesArr = pcode.split('\n');
+	const lineCount = linesArr.length;
+	
+	for(let i=0;i<lineCount;i++)
+	{
+		const line = linesArr[i];
+		const words = line.split(' ');
+		const wordsCount = words.length; 
+		
+		for(let j=0;j<wordsCount;j++)
+		{
+			const word = words[j];
+			if(word == '=')
+			{
+				const name = words[j-1];
+				const value = removeUnwantedCharacter(words[j+1]);
+				const type = 'variable';
+				const token = {type:type,name:name, value:value};
+				tokensArr.push(token);
+			}
+		}
+	}
+
+	return tokensArr;
+}
+
+function createAST(pcode)
+{
+	//var ast = "";
+	//const ast = filbert.parse(pcode, { locations: true, ranges: true });
+	//ast = filbert.parse(pcode);
+	//var ast_damn = filbert_loose.parse_dammit(pcode);
+	//if(ast != null)traverseAST(ast);
+}
+
+
+function traverseAST(obj)
+{
+	const window = vscode.window;
+	const limit = obj.body.length;
+
+	for(let i=0;i<limit;i++)
+	{
+		let node = obj.body[i];
+		window.showInformationMessage(node);
+
+		if(node.type === "VariableDeclaration")
+		{
+			console.log(node.declarations.id.name);
+			window.showInformationMessage(node.declarations.id.name);
+			if(node.init.type === "Literal")
+			{
+				window.showInformationMessage(node.declarations.init.value);
+				console.log(node.init.value);
+			}
+		}
+		
+		//if(typeof value === 'object') traverseAST(value);
+		//else if(Array.isArray(value)) traverseAST(value);
+	}
+}
 
 
 exports.activate = activate;
